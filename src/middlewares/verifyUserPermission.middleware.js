482fd436dabc7d9242466bb9b6882cd8c3765f18
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-05-13 15:16:56 -0300

    feat: created middleware to verify permission of user

---
+++ src/middlewares/verifyUserPermission.middleware.js
@@ -0,0 +1,19 @@
+import { User } from '../models';
+
+const verifyUserPermissionMiddleware = async (req, res, next) => {
+  const { id } = req.decoded;
+  const loggedUser = await User.findOne({ uuid: id }).exec();
+
+  if (loggedUser.isAdm) return next();
+
+  const { uuid } = req.userByParams;
+
+  if (uuid !== id)
+    return res.status(403).json({
+      message: "You don't have permission to update another user !!!",
+    });
+
+  return next();
+};
+
+export default verifyUserPermissionMiddleware;
