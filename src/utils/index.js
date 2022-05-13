a2d875b2385b89ec46d6f653524614ef630cdfba
Rafael Carvalho <65735479+rafaelocdev@users.noreply.github.com>
2022-05-13 13:17:48 -0300

    refactor: change loop for to spread

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,20 +1,5 @@
-const allowedKeys = [
-  'name',
-  'email',
-  'isAdm',
-  'createdOn',
-  'updatedOn',
-  'uuid',
-];
-
 export const serializeUser = (user) => {
-  const serializedUser = {};
-
-  for (let key in user) {
-    if (allowedKeys.includes(key)) {
-      serializedUser[key] = user[key];
-    }
-  }
+  const { password, _id, ...serializedUser } = user.toJSON();
 
   return serializedUser;
 };
